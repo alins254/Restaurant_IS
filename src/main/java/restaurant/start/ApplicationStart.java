1ffc332819187b0ceff32b010b52f288562c9a55
mada_i <mada.iloie@gmail.com>
2019-11-27 21:39:30 +0200

    Added dependency for jaxa bind

--- src/main/java/restaurant/start/ApplicationStart.java
+++ src/main/java/restaurant/start/ApplicationStart.java
@@ -9,7 +9,7 @@ import restaurant.service.UserService;
 public class ApplicationStart {
 
 	public static void main(String[] args) {
-
+		
 		 UserService userService = new UserService();
 		 User user = new User();
 		 user.setAge(18);
