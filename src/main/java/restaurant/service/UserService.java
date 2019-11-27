d1ef2558b48b5b428c4d880cf806c4d9e2af55cb
mada_i <mada.iloie@gmail.com>
2019-11-27 21:10:08 +0200

    Refactor package name

---
+++ src/main/java/restaurant/service/UserService.java
@@ -0,0 +1,46 @@
+package restaurant.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.collections4.*;
+
+import restaurant.entity.User;
+import restaurant.messages.Messages;
+import restaurant.messages.*;
+import restaurant.repository.UserRepo;
+
+public class UserService {
+
+	private UserRepo userRepo = new UserRepo();
+
+	public String addUser(User user) {
+		List<String> missingFields = new ArrayList<String>();
+		if (user == null) {
+			return "User " + Messages.DEFAULT_NULL_MESSAGE;
+		}
+		if (user.getAge() == null) {
+			missingFields.add("age");
+		}
+		if (StringUtils.isEmpty(user.getName())) {
+			missingFields.add("name");
+		}
+		if (CollectionUtils.isNotEmpty(missingFields)) {
+			return "This field(s) are missing: " + missingFields.stream().collect(Collectors.joining(", "));
+		}
+		if (user.getAge() < 18) {
+			return "Invalid age. Must be greater than 18";
+		}
+
+		user.setId(UUID.randomUUID().toString());
+		user.setCreatedAt(new Date());
+		userRepo.insert(user);
+		return "OK. New user succesfully inserted.";
+
+	}
+
+}
---
+++ src/main/java/restaurant/start/ApplicationStart.java
@@ -0,0 +1,21 @@
+package restaurant.start;
+
+import restaurant.entity.User;
+import restaurant.entity.*;
+import restaurant.repository.*;
+import restaurant.service.UserService;
+//import ro.utcn.is.lab2.view.*;
+
+public class ApplicationStart {
+
+	public static void main(String[] args) {
+
+		
+//		 UserService userService = new UserService();
+//		 User user = new User();
+//		 user.setAge(18);
+//		 user.setName("Tufisi");
+//		 System.out.println(userService.addUser(user));
+		
+	}
+}
