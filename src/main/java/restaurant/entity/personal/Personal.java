b22a7247c9a64b285237482c4612cf1327172136
unknown <mihaela.cosmina98@yahoo.com>
2019-11-28 10:56:41 +0200

    first_classes

--- src/main/java/restaurant/entity/personal/Personal.java
+++ src/main/java/restaurant/entity/personal/Personal.java
@@ -1,4 +1,37 @@
 package restaurant.entity.personal;
 
-public class Personal {
+import java.util.Date;
+
+public abstract class Personal {
+
+    private String name;
+    private Date dateOfEmployment;
+    private Double salary;
+
+    public Personal(){
+        super();
+    }
+
+    
+    
+	public Personal(String name, Double salary) {
+		super();
+		this.name = name;
+		this.dateOfEmployment = new Date();
+		this.salary = salary;
+	}
+
+
+
+	public Personal(String name, Double salary, String role){
+        	if(role.toLowerCase().equals("waiter"))
+        		new Waiter(name, salary);
+        	if(role.toLowerCase().equals("chef"));
+        		new Chef(name, salary);
+    }
+	
+	public Personal newWaiter(String name, Double salary) {
+		return (Personal)(new Waiter(name, salary));
+	}
+
 }
