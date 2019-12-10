591fdccf59eab606f14d94337e0ed720d0f12bfe
mada_i <mada.iloie@gmail.com>
2019-12-11 00:20:00 +0200

    ohwell

---
+++ src/main/java/restaurant/controller/MainController.java
@@ -0,0 +1,44 @@
+package restaurant.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    private double x, y;
+
+    @FXML
+    public void onDragTopBar(MouseEvent event){
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+    @FXML
+    public void onPressTopBar(MouseEvent event){
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
+
+    @FXML
+    public void onCloseWindow(MouseEvent event){
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void onHideWindow(MouseEvent event){
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+
+    }
+}
