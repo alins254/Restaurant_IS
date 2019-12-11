38e1efc82c0dddec69634cf5ff34d4419a6bcf6d
alins254 <alin.scarlat25@yahoo.com>
2019-12-12 00:24:34 +0200

    try

--- src/main/java/restaurant/start/ApplicationStart.java
+++ src/main/java/restaurant/start/ApplicationStart.java
@@ -1,16 +1,7 @@
 package restaurant.start;
 
 import restaurant.entity.User;
-import restaurant.entity.personal.Invoice;
-import restaurant.entity.personal.Personal;
-import restaurant.entity.*;
-import restaurant.entity.personal.Waiter;
-import restaurant.repository.*;
 import restaurant.service.UserService;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 public class ApplicationStart {
 
@@ -20,5 +11,8 @@ public class ApplicationStart {
 		user.setAge(18);
 		user.setName("Tufisi");
 		System.out.println(userService.addUser(user));
+
+		new MainLoginFrame();
+
 	}
 }
