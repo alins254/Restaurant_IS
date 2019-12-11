290787e1cd882f5922560712f43bfb526b307114
unknown <mihaela.cosmina98@yahoo.com>
2019-12-11 23:58:11 +0200

    updates and invoice

---
+++ src/main/java/restaurant/entity/personal/Invoice.java
@@ -0,0 +1,9 @@
+package restaurant.entity.personal;
+
+public class Invoice {
+
+
+    public void generate(){
+
+    }
+}
--- src/main/java/restaurant/entity/personal/Waiter.java
+++ src/main/java/restaurant/entity/personal/Waiter.java
@@ -4,22 +4,60 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Waiter extends Personal {
+public class Waiter implements Personal {
 
-	
-		List tables;
-		
+	private String name;
+	private Date dateOfEmployment;
+	private Double salary;
+	List tables;
+	List invoices;
+
 	public Waiter() {
-		super();
-		// TODO Auto-generated constructor stub
+		this.dateOfEmployment = new Date();
 	}
 
 	public Waiter(String name, Double salary) {
-		super(name, salary);
-		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.salary = salary;
+		this.dateOfEmployment = new Date();
 		tables = new ArrayList<String>();
+		invoices = new ArrayList<String>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Date getDateOfEmployment() {
+		return dateOfEmployment;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public List getTables() {
+		return tables;
+	}
+
+	public List getInvoices() {
+		return invoices;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 
-	
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public void setTables(List tables) {
+		this.tables = tables;
+	}
+
+	public void setInvoices(List invoices) {
+		this.invoices = invoices;
+	}
 }
