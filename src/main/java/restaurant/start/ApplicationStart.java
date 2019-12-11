df19885c5114083a86674c8eb06d6373d6b78b30
alins254 <alin.scarlat25@yahoo.com>
2019-12-12 00:24:34 +0200

    try

--- src/main/java/restaurant/start/ApplicationStart.java
+++ src/main/java/restaurant/start/ApplicationStart.java
@@ -2,16 +2,19 @@ package restaurant.start;
 
 import restaurant.entity.User;
 import restaurant.service.UserService;
+
 import javax.swing.*;
 
 
 public class ApplicationStart {
 
 	public static void main(String[] args) {
+
 		UserService userService = new UserService();
 		User user = new User();
 		user.setAge(18);
 		user.setName("Tufisi");
 		System.out.println(userService.addUser(user));
+
 	}
 }
