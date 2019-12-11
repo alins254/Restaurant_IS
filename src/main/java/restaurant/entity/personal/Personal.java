df180dbaced12a57eec58df96300adf0c7f00d3a
unknown <mihaela.cosmina98@yahoo.com>
2019-12-11 17:37:19 +0200

    1

--- src/main/java/restaurant/entity/personal/Personal.java
+++ src/main/java/restaurant/entity/personal/Personal.java
@@ -6,31 +6,4 @@ public interface Personal {
 
 
 
-    /*
-    public Personal(){
-        super();
-    }
-
-    
-    
-	public Personal(String name, Double salary) {
-		super();
-		this.name = name;
-		this.dateOfEmployment = new Date();
-		this.salary = salary;
-	}
-
-
-
-	public Personal(String name, Double salary, String role){
-        	if(role.toLowerCase().equals("waiter"))
-        		new Waiter(name, salary);
-        	if(role.toLowerCase().equals("chef"));
-        		new Chef(name, salary);
-    }
-	
-	public Personal newWaiter(String name, Double salary) {
-		return (Personal)(new Waiter(name, salary));
-	}
- */
 }
