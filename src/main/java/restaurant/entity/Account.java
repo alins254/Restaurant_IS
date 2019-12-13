6a059c43c5f8f2e0722442071c57f3bbc5dd2e33
alins254 <alin.scarlat25@yahoo.com>
2019-12-13 22:03:57 +0200

    AdminService - Add/Remove/Show User

--- src/main/java/restaurant/entity/Account.java
+++ src/main/java/restaurant/entity/Account.java
@@ -1,5 +1,6 @@
 package restaurant.entity;
 
+<<<<<<< HEAD
 import restaurant.entity.personal.Personal;
 
 import javax.persistence.Column;
@@ -23,21 +24,40 @@ public class Account {
     public Account(String username, String password) {
         this.username = username;
         this.password = password;
+=======
+public class Account {
+    private String username;
+    private String role;
+
+    public Account(String username, String role) {
+        this.username = username;
+        this.role = role;
+>>>>>>> AdminService - Add/Remove/Show User
     }
 
     public String getUsername() {
         return username;
     }
 
+<<<<<<< HEAD
     public String getPassword() {
         return password;
+=======
+    public String getRole() {
+        return role;
+>>>>>>> AdminService - Add/Remove/Show User
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+<<<<<<< HEAD
     public void setPassword(String password) {
         this.password = password;
+=======
+    public void setRole(String role) {
+        this.role = role;
+>>>>>>> AdminService - Add/Remove/Show User
     }
 }
