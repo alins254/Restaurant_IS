80d42f09c6cdd4c964cf40a19569bfca43a2b3ee
cosmina <mihaela.cosmina98@yahoo.com>
2019-12-15 18:00:10 +0200

    main test

--- src/main/java/restaurant/start/ApplicationStart.java
+++ src/main/java/restaurant/start/ApplicationStart.java
@@ -15,13 +15,10 @@ import java.util.List;
 public class ApplicationStart {
 
 	public static void main(String[] args) {
-
-		
 		UserService userService = new UserService();
 		User user = new User();
 		user.setAge(18);
 		user.setName("Tufisi");
 		System.out.println(userService.addUser(user));
-
 	}
 }
