03cb36b5adf76a796ca815ca70b1fd0bd68c603e
Daniel Szabo <daniel.szabo.schuller@gmail.com>
2019-12-15 22:17:49 +0200

    updated stock

--- src/main/java/restaurant/entity/stock/Stock.java
+++ src/main/java/restaurant/entity/stock/Stock.java
@@ -1,5 +1,24 @@
 package restaurant.entity.stock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stock {
-    private
+    private List<ItemQuantity> stock;
+
+    public Stock(){
+        this.stock = new ArrayList<>();
+    }
+
+    public Stock(List<ItemQuantity>stock){
+        this.stock = stock;
+    }
+
+    public List<ItemQuantity> getStock() {
+        return stock;
+    }
+
+    public void setStock(List<ItemQuantity> stock) {
+        this.stock = stock;
+    }
 }
