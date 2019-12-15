8bab393c7c1e38d531fdd9a23f259c6344434f9b
cosmina <mihaela.cosmina98@yahoo.com>
2019-12-15 17:18:02 +0200

    modified chef, invoice

--- src/main/java/restaurant/entity/personal/Chef.java
+++ src/main/java/restaurant/entity/personal/Chef.java
@@ -11,9 +11,43 @@ public class Chef implements Personal {
 	private Double salary;
 	List orders;
 
+	public Chef() {
+		dateOfEmployment = new Date();
+		orders = new ArrayList<String>();
+	}
 
+	public Chef(String name, Double salary){
+		this.name = name;
+		this.salary = salary;
+		dateOfEmployment = new Date();
+		orders = new ArrayList<String>();
+	}
 
+	public String getName() {
+		return name;
+	}
 
-	
-	
+	public Date getDateOfEmployment() {
+		return dateOfEmployment;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public List getOrders() {
+		return orders;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public void setOrders(List orders) {
+		this.orders = orders;
+	}
 }
