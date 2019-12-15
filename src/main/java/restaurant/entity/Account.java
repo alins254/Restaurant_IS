a3801be853913f38cfb0c08c25e881c125bcc6f8
alins254 <alin.scarlat25@yahoo.com>
2019-12-15 22:30:01 +0200

    Validator

--- src/main/java/restaurant/entity/Account.java
+++ src/main/java/restaurant/entity/Account.java
@@ -1,12 +1,12 @@
 package restaurant.entity;
 
-<<<<<<< HEAD
 import restaurant.entity.personal.Personal;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+
 
 public class Account {
     @Id
@@ -24,40 +24,21 @@ public class Account {
     public Account(String username, String password) {
         this.username = username;
         this.password = password;
-=======
-public class Account {
-    private String username;
-    private String role;
-
-    public Account(String username, String role) {
-        this.username = username;
-        this.role = role;
->>>>>>> AdminService - Add/Remove/Show User
     }
 
     public String getUsername() {
         return username;
     }
 
-<<<<<<< HEAD
     public String getPassword() {
         return password;
-=======
-    public String getRole() {
-        return role;
->>>>>>> AdminService - Add/Remove/Show User
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-<<<<<<< HEAD
     public void setPassword(String password) {
         this.password = password;
-=======
-    public void setRole(String role) {
-        this.role = role;
->>>>>>> AdminService - Add/Remove/Show User
     }
 }
