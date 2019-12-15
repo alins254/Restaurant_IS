2cadfe6f0f7a718d59b8d63136cc2042080bc468
cosmina <mihaela.cosmina98@yahoo.com>
2019-12-15 17:37:19 +0200

    appStart initial

--- src/main/java/restaurant/start/ApplicationStart.java
+++ src/main/java/restaurant/start/ApplicationStart.java
@@ -16,50 +16,12 @@ public class ApplicationStart {
 
 	public static void main(String[] args) {
 
-		/*
+		
 		UserService userService = new UserService();
 		User user = new User();
 		user.setAge(18);
 		user.setName("Tufisi");
 		System.out.println(userService.addUser(user));
-		*/
-
-
-		/*
-		Personal p;
-		Waiter w = new Waiter();
-		System.out.println(w.getName());
-		System.out.println(w.getDateOfEmployment());
-		System.out.println(w.getInvoices());
-		System.out.println(w.getSalary());
-
-		w.setName("alabala");
-		w.setSalary(5678.0);
-
-		List l = new ArrayList<String>();
-		l.add("inv1");
-		l.add("inv2");
-		l.add("inv3");
-		l.add("inv4");
-
-		w.setInvoices(l);
-
-		System.out.println();
-
-		System.out.println(w.getName());
-		System.out.println(w.getDateOfEmployment());
-		System.out.println(w.getInvoices());
-		System.out.println(w.getSalary());
-
-
-		System.out.println();
-
-		Date d = new Date();
-		System.out.println(d);
-		*/
-
-		Invoice i = new Invoice();
-		i.generate();
 
 	}
 }
