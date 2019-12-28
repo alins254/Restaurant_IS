d3b27e255a5fc2cf1ba11093d00d2506d1ab45ca
mada_i <mada.iloie@gmail.com>
2019-12-28 18:36:34 +0200

    Add ChefView

--- src/main/java/restaurant/controller/AdminViewController.java
+++ src/main/java/restaurant/controller/AdminViewController.java
@@ -2,21 +2,37 @@ package restaurant.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.*;
 
-import restaurant.entity.menu.Menu;
-
 public class AdminViewController implements Initializable {
     private double x, y;
+
+    private String name = "";
+    private String username = "";
+    private Double salary = 0.0;
+    private String role = "";
+    private boolean isValid = false;
+    private List<String> roleList = new ArrayList<String>(Arrays.asList("waiter", "admin", "chef"));
+
+    @FXML private Pane buttonsPane;
+    @FXML private Pane tablePane;
+    @FXML private DialogPane customDialog;
+    @FXML private TextField nameField;
+    @FXML private TextField usernameField;
+    @FXML private ChoiceBox roleChoice;
+    @FXML private TextField salaryField;
+    @FXML private Button btnCreateUser;
+
 
     //top bar methods
     @FXML
@@ -49,6 +65,7 @@ public class AdminViewController implements Initializable {
     @FXML
     public void hireStaff() {
         System.out.println("Hire");
+        showHirePanel();
     }
 
     @FXML
@@ -71,7 +88,81 @@ public class AdminViewController implements Initializable {
         System.out.println("Inspect Stock");
     }
 
+    @FXML public void createUser() {
+        System.out.println("User " + nameField.getText() + ", " + usernameField.getText() +
+                        ", " + ", role: " + roleList.get(roleChoice.getSelectionModel().getSelectedIndex()) +
+                        ", salary: " + salaryField.getText() + " created.");
+        hideHirePanel();
+        clearFields();
+    }
+
+    @FXML public void discard() {
+        System.out.println("Discard.");
+        hideHirePanel();
+        clearFields();
+    }
+
+    @FXML public void onNameFieldChange(){
+        name = nameField.getText();
+        isUserValid();
+    }
+
+    @FXML public void onUsernameFieldChange(){
+        username = usernameField.getText();
+        isUserValid();
+    }
+
+    @FXML public void onSalaryFieldChange(){
+        try {
+            salary = Double.parseDouble(salaryField.getText());
+        } catch (NullPointerException npe) {
+            System.out.println("Salary field is mandatory.");
+            salary = 0.0;
+        } catch (NumberFormatException nfe) {
+            System.out.println("Salary field must be a number.");
+            salary = 0.0;
+        }
+        isUserValid();
+    }
+
+    public boolean isUserValid() {
+        if (salary < 0.001) {
+            isValid = false;
+        } else if (name.equals("")) {
+            isValid = false;
+        } else if (username.equals("")) {
+            isValid = false;
+        } else {
+            isValid = true;
+        }
+
+        System.out.println("Is valid: "+ isValid);
+        btnCreateUser.setDisable(!isValid);
+        return isValid;
+    }
+
+    public void showHirePanel() {
+        customDialog.setVisible(true);
+        tablePane.setDisable(true);
+        buttonsPane.setDisable(true);
+    }
+
+    public void hideHirePanel (){
+        customDialog.setVisible(false);
+        tablePane.setDisable(false);
+        buttonsPane.setDisable(false);
+    }
+
+    public void clearFields() {
+        nameField.setText("");
+        usernameField.setText("");
+        salaryField.setText("");
+    }
+
     @Override
-    public void initialize(URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
+        customDialog.setVisible(false);
+        btnCreateUser.setDisable(!isValid);
+        roleChoice.getItems().addAll(roleList);
     }
 }
