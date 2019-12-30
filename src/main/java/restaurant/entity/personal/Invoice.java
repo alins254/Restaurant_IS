c5830e9188bf77cc551df4afe71e641c464c3095
cosmina <mihaela.cosmina98@yahoo.com>
2019-12-30 19:33:39 +0200

    bug invoice solved

--- src/main/java/restaurant/entity/personal/Invoice.java
+++ src/main/java/restaurant/entity/personal/Invoice.java
@@ -74,6 +74,5 @@ public class Invoice {
         pw.println(total);
 
         pw.close();
-        
     }
 }
--- src/main/java/restaurant/start/ApplicationStart.java
+++ src/main/java/restaurant/start/ApplicationStart.java
@@ -4,7 +4,6 @@ import restaurant.entity.User;
 import restaurant.service.UserService;
 
 import javax.swing.*;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -15,13 +14,11 @@ public class ApplicationStart {
 
 	public static void main(String[] args) {
 
-
 		UserService userService = new UserService();
 		User user = new User();
 		user.setAge(18);
 		user.setName("Tufisi");
 		System.out.println(userService.addUser(user));
-
 
 	}
 }
