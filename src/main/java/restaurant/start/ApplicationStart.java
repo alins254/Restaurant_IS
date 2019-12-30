86da3a7153d62f20c0233df257998eb902d307b5
cosmina <mihaela.cosmina98@yahoo.com>
2019-12-30 20:08:01 +0200

    invoice app

--- src/main/java/restaurant/start/ApplicationStart.java
+++ src/main/java/restaurant/start/ApplicationStart.java
@@ -1,15 +1,7 @@
 package restaurant.start;
 
 import restaurant.entity.User;
-import restaurant.entity.personal.Invoice;
-import restaurant.entity.personal.Personal;
-import restaurant.entity.*;
-import restaurant.entity.personal.Waiter;
-import restaurant.repository.*;
 import restaurant.service.UserService;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -23,5 +15,6 @@ public class ApplicationStart {
 		user.setAge(18);
 		user.setName("Tufisi");
 		System.out.println(userService.addUser(user));
+
 	}
 }
