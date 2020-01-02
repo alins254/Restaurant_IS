13f3aa1ac8c35cf5caf762b9dc7b5f9ae68a2bcd
alins254 <alin.scarlat25@yahoo.com>
2020-01-02 22:02:53 +0200

    refactoring adminService

--- src/main/java/restaurant/service/AdministratorService.java
+++ src/main/java/restaurant/service/AdministratorService.java
@@ -1,40 +1,55 @@
 package restaurant.service;
 
 import restaurant.entity.Account;
+import restaurant.entity.personal.Chef;
+import restaurant.entity.personal.Personal;
+import restaurant.entity.personal.Waiter;
 import restaurant.repository.AdministratorRepo;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class AdministratorService {
 
     private AdministratorRepo repo;
 
-    public String addNewUser(String username, String password, Object role){
+    public Personal createNewPersonal(String name, Double salary, String type){
+        Personal p;
+        if(type.toLowerCase().equals("waiter")){
+            p = new Waiter(name, salary);
+        }else if(type.toLowerCase().equals("chef")){
+            p = new Chef(name, salary);
+        }else
+            return null;
+        //p.setId(UUID.randomUUID().toString());
+
+        return p;
+    }
+
+    public String addNewUser(String username, String password, Personal personal){
         String message = Validator.loginValid(username, password);
 
         if(!message.equals("VALID"))
             return message;
 
-        if(!Validator.existingAccountRole(role))
-            return "Invalid Role";
-
-        //return repo.addNewUser(username, password, role);
-        return "";
+        Account account = new Account(username, password);
+        //account.setPerson(personal.getId());
+        return repo.addNewUser(account, personal);
     }
 
     public String removeUser(String username){
         String message = Validator.usernameValid(username);
         if(message.equals("")) {
-            //message = repo.removeUser(username);
+            message = repo.removeUser(username);
             return message;
         }
         return message;
     }
 
-    public ArrayList<Account> showAllUser(){
-        ArrayList<Account> accounts = repo.getAllUsers();
+    public ArrayList<Personal> showAllPersonal(){
+        ArrayList<Personal> accounts = (ArrayList<Personal>) repo.showAllPersonal();
         if(accounts == null)
-            return new ArrayList<Account>();
+            return new ArrayList<Personal>();
         return accounts;
     }
 
